30983285a1b30ab50537d1ac92638f845eeeb387
Spencer Seung <sseung@nettheory.com>
2011-04-29 15:42:43 -0400

    added for guest checkout case, disabled functionality if guest checkout

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -6,10 +6,10 @@ CheckoutController.class_eval do
 
   def before_address
     #puts "BEFORE ADDRESS2 called user= #{@order}  #{@order.email}"
-    @order.bill_address , @order.ship_address = FindAddressHelper.find_address(@order.email)
+    @order.bill_address , @order.ship_address = FindAddressHelper.find_address(@order.email) unless @order.user.anonymous?
     # in case none found
-    @order.bill_address ||= current_user.bill_address || Address.new(:country_id => default_country_id)
-    @order.ship_address ||= current_user.ship_address || Address.new(:country_id => default_country_id)
+    @order.bill_address ||= current_user.try(:bill_address) || Address.new(:country_id => default_country_id)
+    @order.ship_address ||= current_user.try(:ship_address) || Address.new(:country_id => default_country_id)
   end
 
 
